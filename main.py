2cf5998acbb80d32253f8b08550cbd289b1746d3
bobbyhadz <opmftw@gmail.com>
2024-04-10 16:23:35 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Why does list.reverse() return None in Python
+
+a_list = ['bobby', 'hadz', '.', 'com']
+
+result = a_list.reverse()
+print(result)  # 👉️ None
+
+print(a_list)  # 👉️ ['com', '.', 'hadz', 'bobby']
